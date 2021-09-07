cadd670da3ea1af52c53d498bba860f8213072a6
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-07 14:38:06 +0100

    first version of script to move all maker files to specified directory. Very manual. Must be run within datastore directory. Can modify for further automation at some stage.

---
+++ parse_maker_logfile.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+my $maker_pred_dir = "/home/people/16386986/scratch/MAAGS_Vocalisation_Chemosensation/Genbank_VomVoc_annotation/Primate_Assemblies_03_Aug_21_GenBank_Rep_Genomes/Maker_predictions/";
+
+my $file_extension = "master_datastore_index.log";
+my @logfile_array = (<*$file_extension>);
+
+foreach my $file(@logfile_array) {
+    open(IN, $file);
+    while(<IN>){
+	my $line = $_;
+	if ($line =~ m/.*?\s(.*)?\t.*FINISHED/i){
+	    my $dir = $1;
+	    if ($dir =~ m/(GC.*)?_/i){
+		my $genome = $1;
+	    }
+	    #print $dir."\n";
+	    my $dir_files = $dir."*fasta";
+	    system("cp $dir_files $maker_pred_dir");
+	}
+    }
+    my $gendir = $maker_pred_dir.$genome;
+    system("mkdir $gendir");
+    my $maker_pred_fasta= $maker_pred_dir."*.fasta";
+    system("mv $maker_pred_fasta $gendir");
+}
+
