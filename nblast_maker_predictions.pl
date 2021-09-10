8ce8690cf6a037f68516b4fd03f5438af76f2de0
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-10 15:31:09 +0100

    Changed output annotation to csv format

--- nblast_maker_predictions.pl
+++ nblast_maker_predictions.pl
@@ -103,7 +103,7 @@ foreach my $GENOME(@genome_array) {
 				$Gene2 = $Gene;
 				$Gene2 =~ s/split//;
 				$Gene_Annotation = $Gene_Annotation.$Gene_Query."|".$Gene2."\n";
-				$Gene_Annotation_Summary = $Gene_Annotation_Summary.$Gene2."|".$Gene_Query."\n";
+				$Gene_Annotation_Summary = $Gene_Annotation_Summary.$Gene_Annotation.",".$Gene_Query."\n";
 			    }
 			    
 		#	}
@@ -171,7 +171,7 @@ foreach my $GENOME(@genome_array) {
 	  }
 	}
 	close GENOME;
-	my $Summary_File = $GENOME_ID."_Gene_Annotation_Summary";
+	my $Summary_File = $GENOME_ID."_Gene_Annotation_Summary.csv";
 	open my $SFile, ">", $Summary_File or die("Can't open file. $!");
 	print $SFile $Gene_Annotation_Summary;
 	close $SFile;
