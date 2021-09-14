89288a7e899183f0c2acaace21579d18e1d33a53
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-14 16:40:34 +0100

    Adding cat maker files to merge maker contig prediction fastas to singhle files which can then be used for nblast maker predictions.

---
+++ cat_maker_files.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+print "\nMerging maker predictions to output files ..\n\n";
+
+#1 Store genome accession:
+use Cwd qw(cwd);
+my $dir = cwd;
+if ($dir =~ m/.*(GC.*?\_.*?\_).*/i) {
+    my $GENOME = $1;
+}
+
+#Maker Proteins 
+my $Maker_proteins_extension = "maker_proteins.fasta";
+my $Maker_proteins = "maker_proteins_fasta";
+my $Maker_proteins_file = $GENOME.$Maker_proteins;
+
+#Maker transcripts
+my $Maker_transcripts_extension = "maker_transcripts.fasta";
+my $Maker_transcripts = "maker_transcripts_fasta";
+my $Maker_transcripts_file = $GENOME.$Maker_transcripts;
+
+#Maker abinitio proteins
+my $Maker_AbInitio_proteins_extension = "maker_non_overlapping_ab_initio_proteins.fasta";
+my $Maker_AbInitio_proteins = "maker_non_overlapping_ab_initio_proteins_fasta"
+my $Maker_AbInitio_proteins_file = $GENOME.$Maker_AbInitio_proteins;
+
+#Maker abinitio transcripts
+my $Maker_AbInitio_transcripts_extension = "maker_non_overlapping_ab_initio_transcripts.fasta";
+my $Maker_AbInitio_transcripts = "maker_non_overlapping_ab_initio_transcripts_fasta";
+my $Maker_AbInitio_transcripts_file = $GENOME.$Maker_AbInitio_transcripts;
+
+#Maker augustus proteins
+my $Maker_Augustus_proteins_extension = "augustus_masked_proteins.fasta";
+my $Maker_Augustus_proteins = "augustus_masked_proteins_fasta";
+my $Maker_Augustus_proteins_file = $GENOME.$Maker_Augustus_proteins;
+
+#Maker augustus transcripts
+my $Maker_Augustus_transcripts_extension ="augustus_masked_transcripts.fasta";
+my $Maker_Augustus_transcripts = "augustus_masked_transcripts_fasta";
+my $Maker_Augustus_transcripts_file = $GENOME.$Maker_Augustus_transcripts;
+
+
+#2 Merge_fasta_files.pl
+
+#Maker proteins
+system("merge_fasta_files.pl $Maker_proteins_extension $Maker_proteins_file");
+
+#Maker transcripts
+system("merge_fasta_files.pl $Maker_transcripts_extension $Maker_transcripts_file");
+
+#Maker AbInitio proteins
+system("merge_fasta_files.pl $Maker_AbInitio_proteins_extension $Maker_AbInitio_proteins_file");
+
+#Maker AbInitio transcripts
+system("merge_fasta_files.pl $Maker_AbInitio_transcripts_extension $Maker_AbInitio_transcripts_file");
+
+#Maker Augustus proteins
+system("merge_fasta_files.pl $Maker_Augustus_proteins_extension $Maker_Augustus_proteins_file");
+
+#Maker Augustus transcripts
+system("merge_fasta_files.pl $Maker_Augustus_transcripts_extension $Maker_Augustus_transcripts_file");
+
+
+#3 Make raw files directory and move all raw files here
+
+my $RAWDIR = "RAW_MAKER_FILES";
+system("mkdir $RAWDIR");
+system("mv *fasta $RAWDIR");
+
+#4 Move protein files to directory
+
+my $PROT = "PROTEIN_DIRECTORY";
+system("mv *$Maker_proteins $PROT");
+system("mv *$Maker_AbInitio_proteins $PROT");
+system("mv *$Maker_Augustus_proteins $PROT");
+
+print "Done!\n";
+
+exit;
