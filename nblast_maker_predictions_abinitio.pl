d8261ebad8eac9732ae4604d4342404ac41291a5
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-14 15:22:20 +0100

    Remove long headers

--- nblast_maker_predictions_abinitio.pl
+++ nblast_maker_predictions_abinitio.pl
@@ -299,7 +299,7 @@ foreach my $GENOME(@genome_array) {
 		 chomp;
 		 $gene_seq = $_; #store each gene  of the genome file in $gene_seq
 		 if ($gene_seq =~ m/$Prediction\s\(.*\)(.*)?\n/i) { #if gene is a match, pull the gene sequence from genome file
-		     $gene_seq =~ s/$1//g;
+		     $gene_seq =~ s/\Q$1\E//g;
 		     $GENE_HIT = $GENE_HIT.">".$Annotation."_".$GENOME_ID."_".$gene_seq."\n";
 		 }
 	     }
