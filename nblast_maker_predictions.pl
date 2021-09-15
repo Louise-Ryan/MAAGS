ff27d8c6e4f16337f4154cc854e2e96768ca6e63
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-15 16:14:11 +0100

    Ok maybe that gene_chunk fix did work. Lets try this again

--- nblast_maker_predictions.pl
+++ nblast_maker_predictions.pl
@@ -264,23 +264,23 @@ foreach my $GENOME(@genome_array) {
 		    }
 		    $Gene =~ s/split//;
 		    $Gene_Hit_Summary = $Gene_Hit_Summary.$Gene.", "; #Store gene identifier in summary file
-		    if ($line =~ m/(Score\s\=.*?\,)/i) { 
+		    if ($gene_chunk =~ m/(Score\s\=.*?\,)/i) { 
 			$Score = $1; #Store score (bits) info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Score;
 		    }
-		    if ($line =~ m/(Expect\s\=.*[0-9].*e.*[0-9])/i) { 
+		    if ($gene_chunk =~ m/(Expect\s\=.*[0-9].*e.*[0-9])/i) { 
 			$EValue = $1; #Store e-value score info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary." ".$EValue.", ";
 		    }
-		    if ($line =~ m/(Identities\s\=.*?\,)/i) { 
+		    if ($gene_chunk =~ m/(Identities\s\=.*?\,)/i) { 
 			$Identity = $1; #Store Identity score in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Identity;
 		    }
-		    if ($line =~ m/(Gaps\s\=.*?\))/i) {
+		    if ($gene_chunk =~ m/(Gaps\s\=.*?\))/i) {
 			$Gaps = $1; #Store Gap info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary." ".$Gaps.", ";
 		    }
-		    if ($line =~ m/(Strand\=.*\/.*?\n)/i) { 
+		    if ($gene_chunk =~ m/(Strand\=.*\/.*?\n)/i) { 
 			$Strand = $1; #Store strand info in summary file
 			$Strand =~ s/\n//;
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Strand;
