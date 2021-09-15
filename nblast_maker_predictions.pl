59466c89b50099480f33d0474f9ebc2efd627147
Louise <Louise.ryan1@ucdconnect.ie>
2021-09-15 15:43:03 +0100

    Blast summary files are not correct. For each gene query, printing same scores to all. Should be storing unique scores to unique hits per query. Changing if  matches to if  matches within the blast summary chunks. Lets see if this works, if so I will do this for all blast chunks.

--- nblast_maker_predictions.pl
+++ nblast_maker_predictions.pl
@@ -264,23 +264,23 @@ foreach my $GENOME(@genome_array) {
 		    }
 		    $Gene =~ s/split//;
 		    $Gene_Hit_Summary = $Gene_Hit_Summary.$Gene.", "; #Store gene identifier in summary file
-		    if ($line =~ m/(Score\s\=.*?\,)/i) {
+		    if ($gene_chunk=~ m/(Score\s\=.*?\,)/i) { #changed
 			$Score = $1; #Store score (bits) info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Score;
 		    }
-		    if ($line =~ m/(Expect\s\=.*[0-9].*e.*[0-9])/i) {
+		    if ($gene_chunk =~ m/(Expect\s\=.*[0-9].*e.*[0-9])/i) { #changed
 			$EValue = $1; #Store e-value score info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary." ".$EValue.", ";
 		    }
-		    if ($line =~ m/(Identities\s\=.*?\,)/i) {
+		    if ($gene_chunk =~ m/(Identities\s\=.*?\,)/i) { #chanhed
 			$Identity = $1; #Store Identity score in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Identity;
 		    }
-		    if ($line =~ m/(Gaps\s\=.*?\))/i) {
+		    if ($gene_chunk =~ m/(Gaps\s\=.*?\))/i) { #changed
 			$Gaps = $1; #Store Gap info in summary file
 			$Gene_Hit_Summary = $Gene_Hit_Summary." ".$Gaps.", ";
 		    }
-		    if ($line =~ m/(Strand\=.*\/.*?\n)/i) {
+		    if ($gene_chunk =~ m/(Strand\=.*\/.*?\n)/i) { #changed $line to $gene_chunk
 			$Strand = $1; #Store strand info in summary file
 			$Strand =~ s/\n//;
 			$Gene_Hit_Summary = $Gene_Hit_Summary.$Strand;
