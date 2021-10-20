f906c2148636932e8102211f209afb0c158153fb
Louise <Louise.ryan1@ucdconnect.ie>
2021-10-20 15:16:37 +0100

    mod

--- compare_lists.pl
+++ compare_lists.pl
@@ -3,8 +3,8 @@
 my $file1 = $ARGV[0];
 my $file2 = $ARGV[1];
 
-my @array1 = (<$file1>);
-my @array2 = (<$file2>);
+my @array1 = (<*$file1>);
+my @array2 = (<*$file2>);
 
 my $store;
 foreach my $element(@array1) {
@@ -12,7 +12,7 @@ foreach my $element(@array1) {
 }
 print $store."\n";
 
-foreach my $entry(@array1) {
+foreach my $entry(@array2) {
     if ($store !~ m/.*$entry.*/i) {
 	print $entry;
     }
