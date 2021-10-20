4b77b8da5e4fdd64f56c7bf6a28d67e5eba222f0
Louise <Louise.ryan1@ucdconnect.ie>
2021-10-20 15:14:52 +0100

    mod

--- compare_lists.pl
+++ compare_lists.pl
@@ -3,8 +3,8 @@
 my $file1 = $ARGV[0];
 my $file2 = $ARGV[1];
 
-my @array1 = <$file1>;
-my @array2 = <$file2>;
+my @array1 = (<$file1>);
+my @array2 = (<$file2>);
 
 my $store;
 foreach my $element(@array1) {
