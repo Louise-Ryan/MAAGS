2b91a2984e3fd3ceda87323f92f686ca68c5ba43
Louise <Louise.ryan1@ucdconnect.ie>
2021-10-20 15:19:52 +0100

    mod

--- compare_lists.pl
+++ compare_lists.pl
@@ -3,8 +3,13 @@
 my $file1 = $ARGV[0];
 my $file2 = $ARGV[1];
 
-my @array1 = (<*$file1>);
-my @array2 = (<*$file2>);
+open(IN, $file1);
+@array1 = <IN>;
+close IN;
+
+open(IN2, $file2);
+@array2 = <IN2>;
+close IN2;
 
 my $store;
 foreach my $element(@array1) {
