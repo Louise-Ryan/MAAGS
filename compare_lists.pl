866fa2dd48f4d6754a265b1b493854ab06c311e0
Louise <Louise.ryan1@ucdconnect.ie>
2021-10-20 17:11:15 +0100

    mod

--- compare_lists.pl
+++ compare_lists.pl
@@ -16,23 +16,22 @@ $array2 =~ s/\n//g;
 #$array2 = "|".$array2."|";
 #print $array2;
 
-#my $store;
-#foreach my $element(@array1) {
- #   $store = $store.$element."_";
-#}
-#print $store."\n";
+my $store;
 
 foreach my $entry(@array1) {
     $entry =~ s/\n//g;
     $entry =~ s/\s//g;
 #    print $entry;
     if ($array2 !~ m/$entry/i){
-	print "\n\n".$entry."neq";
+	#print "\n\n".$entry."is not in gene alignment";
+	$store = $store.$entry."\n";
     }elsif ($array2 =~ m/$entry/i) {
 #	print "\n\n".$entry."eq";
     }else{
 	print "\nwtf is going on?\n";
     }
 }
+
+print $store;
 
 exit;
